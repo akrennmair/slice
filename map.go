c559057a198c99d4a7f2088fd5e052d0ffec32be
Andreas Krennmair <ak@synflood.at>
2022-01-02 18:55:11 +0100

    improve map performance by preallocating output to expected size.

--- map.go
+++ map.go
@@ -8,6 +8,7 @@ import (
 // Map returns a new slice populated with the result of calling the provided function
 // on every element in the provided input slice.
 func Map[T1, T2 any](input []T1, f func(T1) T2) (output []T2) {
+	output = make([]T2, 0, len(input))
 	for _, v := range input {
 		output = append(output, f(v))
 	}
