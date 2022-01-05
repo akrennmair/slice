49445747ab8191e4cb347d8e2d489663f71a84f0
Andreas Krennmair <ak@synflood.at>
2022-01-05 21:38:17 +0100

    add ReduceWithInitialValue.

--- reduce.go
+++ reduce.go
@@ -13,3 +13,16 @@ func Reduce[T1, T2 any](input []T1, f func(T2, T1) T2) T2 {
 
 	return acc
 }
+
+// ReduceWithInitialValue executes a provided function on each element of the slice
+// in order, passing the return value of the previous function call
+// on the preceding slice element. Unlike Reduce, the initial value of
+// the accumulator can be provided as an argument. The final result of running the
+// provided function across all slice elements is returned.
+func ReduceWithInitialValue[T1, T2 any](input []T1, acc T2, f func(T2, T1) T2) T2 {
+	for _, v := range input {
+		acc = f(acc, v)
+	}
+
+	return acc
+}
--- reduce_test.go
+++ reduce_test.go
@@ -19,3 +19,16 @@ func TestReduce(t *testing.T) {
 
 	require.Equal(t, int16(4950), sum)
 }
+
+func TestReduceWithInitialValue(t *testing.T) {
+	var testData []int
+	for i := 1; i <= 10; i++ {
+		testData = append(testData, i)
+	}
+
+	result := slice.ReduceWithInitialValue(testData, 1, func(acc int, v int) int {
+		return acc * v
+	})
+
+	require.Equal(t, 3628800, result)
+}
